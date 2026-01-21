0afa733c234eabb4f279fb0d87ed10e25aac3fef
codingkimrizki <rizkiardiant0005@gmail.com>
2026-01-21 07:08:14 +0700

    showing modal in major button

--- src/views/master/machine/component/MachineList.vue
+++ src/views/master/machine/component/MachineList.vue
@@ -12,22 +12,45 @@
     @change="onTableChange"
   >
     <!-- SLOT STATUS -->
-    <template #status="{ text }">
-      <a-button v-if="text === 'major'" type="primary" danger size="small">
-        Major
+    <template #status="{ text, record }">
+      <a-button
+        v-if="text === 'major'"
+        type="primary"
+        danger
+        size="small"
+        @click="handleAction(record)"
+      >
+        MAJOR
       </a-button>
 
       <a-button v-else-if="text === 'clear'" type="primary" size="small">
-        Clear
+        CLEAR
       </a-button>
 
       <span v-else>-</span>
     </template>
   </BaseTable>
+  <Teleport to="body">
+    <BaseModal
+      :visible="modal.visible"
+      @close="handleClose"
+      :modalTitle="modal.title"
+    >
+      <template #body>
+        <div style="line-height: 1.5">
+          <p><strong>IP Address:</strong> {{ modal.data?.ipAddress || '-' }}</p>
+          <p><strong>Date:</strong> {{ modal.data?.date || '-' }}</p>
+          <p><strong>Time:</strong> {{ modal.data?.time || '-' }}</p>
+          <p><strong>Status:</strong> {{ modal.data?.status || '-' }}</p>
+        </div>
+      </template>
+    </BaseModal>
+  </Teleport>
 </template>
 
 <script setup>
 import BaseTable from '@/components/base/BaseTable.vue'
+import BaseModal from '@/components/base/BaseModal.vue'
 import { ref, computed, onMounted, watch } from 'vue'
 import { debounce } from 'lodash-es'
 import { useTabelAnswersStore } from '@/stores/tabelAnswers'
@@ -102,4 +125,22 @@ onMounted(async () => {
   await machineStore.get()
   loading.value = false
 })
+
+const modal = ref({
+  visible: false,
+  title: '',
+  data: null, // data baris yang diklik
+})
+
+const handleAction = (record = null) => {
+  modal.value.visible = true
+  modal.value.title = 'Major Alert'
+  modal.value.data = record
+}
+
+const handleClose = () => {
+  modal.value.visible = false
+  modal.value.data = null
+  modal.value.title = ''
+}
 </script>
