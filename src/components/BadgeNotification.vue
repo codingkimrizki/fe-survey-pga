f1d9d87b91607fc891de3b09bc0a91237eb903ce
Rizki Ardianto <Rizki.Ardianto@indonesia.hirose.co.id>
2026-01-26 12:57:27 +0700

    change perubahan2

--- src/components/BadgeNotification.vue
+++ src/components/BadgeNotification.vue
@@ -1,40 +1,16 @@
 <template>
-  <!-- BADGE -->
-  <a-badge :count="store.countNotification" :show-zero="false" :offset="[0, 6]">
-    <a-button type="dashed" shape="circle" @click="store.toggleDrawer('major')">
+  <a-badge :count="store.countNotification">
+    <a-button type="dashed" shape="circle">
       <span class="items-center">
         <BellOutlined />
       </span>
     </a-button>
   </a-badge>
-
-  <!-- DRAWER -->
-  <a-drawer
-    title="Major Notifications"
-    :open="store.drawer.open"
-    @close="store.toggleDrawer()"
-  >
-    <div v-if="store.majorNotifications.length">
-      <div v-for="n in store.majorNotifications" :key="n.id" class="mb-2">
-        🚨 {{ n.message }}
-      </div>
-    </div>
-
-    <div v-else>
-      <BaseEmpty />
-    </div>
-  </a-drawer>
 </template>
 
 <script setup>
-import { onMounted } from 'vue'
 import { BellOutlined } from '@ant-design/icons-vue'
-import { useAnswersStore } from '@/stores/answers'
-import { BaseEmpty } from '@/components/base/BaseEmpty.vue'
+import { useMaintenanceNotifyStore } from '@/stores/maintenanceNotify'
 
-const store = useAnswersStore()
-
-onMounted(() => {
-  store.fetchAnswers()
-})
+const store = useMaintenanceNotifyStore()
 </script>
